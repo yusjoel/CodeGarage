f272e6920c143f490441e8f901ec67946d9ebf64
yusjoel <joelyusj@hotmail.com>
2020-03-30 16:57:09 +0800

    Add c2utf8

---
+++ UniversalCharDet/c2uft8/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mozilla.NUniversalCharDet;
+using System.IO;
+
+namespace c2utf8
+{
+    class Program
+    {
+        static void help()
+        {
+            Console.WriteLine("c2utf8 = convert to utf-8 file");
+            Console.WriteLine("Usage: c2utf8 path pattern");
+            Console.WriteLine("Example: c2utf8 c:/scripts *.cs");
+            Console.WriteLine();
+        }
+
+        static int Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                help();
+                return -1;
+            }
+
+            string path = args[0];
+            if (!Directory.Exists(path))
+            {
+                help();
+                Console.WriteLine("Error: Directory {0} not exists.", path);
+                return -1;
+            }
+
+            string searchPattern = args[1];
+            string[] files = Directory.GetFiles(path, searchPattern, SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                convert(file);
+            }
+
+            return 0;
+        }
+
+        static void convert(string file)
+        {
+            string stringEncoded = null;
+            using (BinaryReader br = new BinaryReader(File.OpenRead(file)))
+            {
+                int length = (int)br.BaseStream.Length;
+                byte[] buffer = br.ReadBytes(length);
+                UniversalDetector uDetecter = new UniversalDetector(null);
+
+                uDetecter.HandleData(buffer, 0, length);
+
+                uDetecter.DataEnd();
+                string detectedCharset = uDetecter.GetDetectedCharset();
+                if (string.IsNullOrEmpty(detectedCharset))
+                {
+                    Console.WriteLine("Warning: {0} not detected", file);
+                }
+                else
+                {
+                    Console.WriteLine("Detected: {0} - {1}", file, detectedCharset);
+                    if (detectedCharset != "UTF-8")
+                    {
+                        Encoding encoding = Encoding.GetEncoding(detectedCharset);
+                        stringEncoded = encoding.GetString(buffer);
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(stringEncoded))
+            {
+                using(StreamWriter sw = new StreamWriter(File.Open(file, FileMode.Create), Encoding.UTF8))
+                {
+                    sw.Write(stringEncoded);
+                }
+            }
+
+        }
+    }
+}
---
+++ UniversalCharDet/readme.md
@@ -0,0 +1,8 @@
+# c2utf8
+检查文本文件的格式, 如果不是UTF8, 自动转成UTF8
+
+> c2utf8 = convert to utf-8 file
+> Usage: c2utf8 path pattern
+> Example: c2utf8 c:/scripts *.cs
+
+只是简单的调用[NUniversalCharDet](https://github.com/he1a2s0/nuniversalchardet)
