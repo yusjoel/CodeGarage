f6b53fedb4919ca058bff66d7f0062805a498cb1
yusjoel <joelyusj@hotmail.com>
2020-03-30 16:10:54 +0800

    ApkRenamer

---
+++ ApkRenamer/ApkRenamer/Program.cs
@@ -0,0 +1,175 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using Iteedee.ApkReader;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+
+namespace ApkRenamer
+{
+    internal class Program
+    {
+        private static readonly string[] InvalidChars = new[]
+        {
+            "\\", "/", ":", "*", "?", "<", ">", "|"
+        };
+
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                PrintHelp();
+                Environment.Exit(-1);
+            }
+
+            string apkPath = args[0];
+
+            if (!File.Exists(apkPath))
+            {
+                Console.WriteLine("File not exists: " + apkPath);
+                PrintHelp();
+                Environment.Exit(-1);
+            }
+
+            string directory = Path.GetDirectoryName(apkPath);
+            string extension = Path.GetExtension(apkPath);
+            if (directory == null || extension == null)
+            {
+                Console.WriteLine("This is not an apk/xapk file: " + apkPath);
+                PrintHelp();
+                Environment.Exit(-1);
+            }
+
+            extension = extension.ToLower();
+            if (extension != ".apk" && extension != ".xapk")
+            {
+                Console.WriteLine("This is not an apk/xapk file: " + apkPath);
+                PrintHelp();
+                Environment.Exit(-1);
+            }
+
+            string pattern = "|label|(|version-name|)" + extension;
+            if (args.Length > 1)
+            {
+                pattern = args[1];
+                if (!pattern.ToLower().EndsWith(extension))
+                    pattern = pattern + extension;
+            }
+
+            if (extension == ".apk")
+                RenameApk(apkPath, pattern, directory);
+            else if (extension == ".xapk")
+                RenameXapk(apkPath, pattern, directory);
+        }
+
+        private class XapkManifest
+        {
+            public string package_name;
+            public string name;
+            public string version_code;
+            public string version_name;
+        }
+
+        private static void RenameXapk(string xapkPath, string pattern, string directory)
+        {
+            XapkManifest xapkManifest = null;
+            using (var fileStream = File.OpenRead(xapkPath))
+            using(var zipFile = new ZipFile(fileStream))
+            {
+                try
+                {
+                    foreach (ZipEntry zipEntry in zipFile)
+                    {
+                        if (zipEntry.IsDirectory) continue;
+                        string entryName = zipEntry.Name.ToLower();
+
+                        if (entryName == "manifest.json")
+                        {
+                            var manifestData = new byte[50 * 1024];
+                            using (var stream = zipFile.GetInputStream(zipEntry))
+                                stream.Read(manifestData, 0, manifestData.Length);
+
+                            xapkManifest = JsonConvert.DeserializeObject<XapkManifest>(Encoding.UTF8.GetString(manifestData));
+                            break;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    PrintHelp();
+                    Environment.Exit(-1);
+                }
+            }
+
+            if (xapkManifest == null)
+                return;
+
+            pattern = pattern.Replace("|label|", xapkManifest.name);
+            pattern = pattern.Replace("|application-name|", xapkManifest.name);
+            pattern = pattern.Replace("|name|", xapkManifest.name);
+            pattern = pattern.Replace("|version-name|", xapkManifest.version_name);
+            pattern = pattern.Replace("|version-code|", xapkManifest.version_code);
+            pattern = pattern.Replace("|package-name|", xapkManifest.package_name);
+            foreach (string invalidChar in InvalidChars)
+                pattern = pattern.Replace(invalidChar, " ");
+
+            string newApkPath = Path.Combine(directory, pattern);
+            try
+            {
+                File.Move(xapkPath, newApkPath);
+                Console.Write("{0} => {1}\n", xapkPath, newApkPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                PrintHelp();
+                Environment.Exit(-1);
+            }
+        }
+
+        private static void RenameApk(string apkPath, string pattern, string directory)
+        {
+            ApkInfo apkInfo = null;
+            try
+            {
+                apkInfo = ReadApk.ReadApkFromPath(apkPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                PrintHelp();
+                Environment.Exit(-1);
+            }
+
+            pattern = pattern.Replace("|label|", apkInfo.label);
+            pattern = pattern.Replace("|application-name|", apkInfo.label);
+            pattern = pattern.Replace("|name|", apkInfo.label);
+            pattern = pattern.Replace("|version-name|", apkInfo.versionName);
+            pattern = pattern.Replace("|version-code|", apkInfo.versionCode);
+            pattern = pattern.Replace("|package-name|", apkInfo.packageName);
+            foreach (string invalidChar in InvalidChars)
+                pattern = pattern.Replace(invalidChar, " ");
+
+            string newApkPath = Path.Combine(directory, pattern);
+            try
+            {
+                File.Move(apkPath, newApkPath);
+                Console.Write("{0} => {1}\n", apkPath, newApkPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                PrintHelp();
+                Environment.Exit(-1);
+            }
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("Usage: ApkRenamer apk-path [pattern]");
+            Console.WriteLine("pattern: |label| or |application-name| or |name|, |version-name|, |version-code|, |package-name|");
+            Console.WriteLine("Default pattern is |label|(|version-name|).apk");
+        }
+    }
+}
---
+++ ApkRenamer/ApkRenamer/ReadApk.cs
@@ -0,0 +1,136 @@
+﻿using System.IO;
+using Ionic.Zip;
+using Iteedee.ApkReader;
+
+namespace ApkRenamer
+{
+    public class ReadApk
+    {
+        public static ApkInfo ReadApkFromPath(string path)
+        {
+            byte[] manifestData = null;
+            byte[] resourcesData = null;
+#if false
+            using (ICSharpCode.SharpZipLib.Zip.ZipInputStream zip = new ICSharpCode.SharpZipLib.Zip.ZipInputStream(File.OpenRead(path)))
+            {
+                using (var filestream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    ICSharpCode.SharpZipLib.Zip.ZipFile zipfile = new ICSharpCode.SharpZipLib.Zip.ZipFile(filestream);
+                    ICSharpCode.SharpZipLib.Zip.ZipEntry item;
+                    while ((item = zip.GetNextEntry()) != null)
+                    {
+                        if (item.Name.ToLower() == "androidmanifest.xml")
+                        {
+                            manifestData = new byte[50 * 1024];
+                            using (Stream strm = zipfile.GetInputStream(item))
+                            {
+                                strm.Read(manifestData, 0, manifestData.Length);
+                            }
+
+                        }
+                        if (item.Name.ToLower() == "resources.arsc")
+                        {
+                            using (Stream strm = zipfile.GetInputStream(item))
+                            {
+                                using (BinaryReader s = new BinaryReader(strm))
+                                {
+                                    resourcesData = s.ReadBytes((int)s.BaseStream.Length);
+
+                                }
+                            }
+                        }
+
+                        if (resourcesData != null && manifestData != null)
+                            break;
+                    }
+                }
+            }
+#elif false
+            var fileStream = File.OpenRead(path);
+            var zipFile = new ZipFile(fileStream);
+            foreach (ZipEntry zipEntry in zipFile)
+            {
+                if(zipEntry.IsDirectory) continue;
+                string entryName = zipEntry.Name.ToLower();
+                if (entryName == "androidmanifest.xml")
+                {
+                    manifestData = new byte[50 * 1024];
+                    using (var stream = zipFile.GetInputStream(zipEntry))
+                    {
+                        stream.Read(manifestData, 0, manifestData.Length);
+                    }
+
+                }
+                else if (entryName == "resources.arsc")
+                {
+                    using (var stream = zipFile.GetInputStream(zipEntry))
+                    {
+                        using (var binaryReader = new BinaryReader(stream))
+                        {
+                            resourcesData = binaryReader.ReadBytes((int)binaryReader.BaseStream.Length);
+                        }
+                    }
+                }
+            }
+#else
+            using (var zipFile = ZipFile.Read(path))
+            {
+                foreach (var zipEntry in zipFile)
+                {
+                    if (zipEntry.IsDirectory) continue;
+                    string entryName = zipEntry.FileName.ToLower();
+                    if (entryName == "androidmanifest.xml")
+                    {
+                        using (var stream = new MemoryStream())
+                        {
+                            zipEntry.Extract(stream);
+                            manifestData = stream.ToArray();
+                        }
+                    }
+                    else if (entryName == "resources.arsc")
+                    {
+                        using (var stream = new MemoryStream())
+                        {
+                            zipEntry.Extract(stream);
+                            resourcesData = stream.ToArray();
+                        }
+                    }
+                }
+            }
+
+
+#endif
+
+            var apkReader = new ApkReader();
+            var info = apkReader.extractInfo(manifestData, resourcesData);
+#if false
+            Console.WriteLine(string.Format("Package Name: {0}", info.packageName));
+            Console.WriteLine(string.Format("Version Name: {0}", info.versionName));
+            Console.WriteLine(string.Format("Version Code: {0}", info.versionCode));
+
+            Console.WriteLine(string.Format("App Has Icon: {0}", info.hasIcon));
+            if(info.iconFileName.Count > 0)
+                Console.WriteLine(string.Format("App Icon: {0}", info.iconFileName[0]));
+            Console.WriteLine(string.Format("Min SDK Version: {0}", info.minSdkVersion));
+            Console.WriteLine(string.Format("Target SDK Version: {0}", info.targetSdkVersion));
+
+            if (info.Permissions != null && info.Permissions.Count > 0)
+            {
+                Console.WriteLine("Permissions:");
+                info.Permissions.ForEach(f =>
+                {
+                    Console.WriteLine(string.Format("   {0}", f));
+                });
+            }
+            else
+                Console.WriteLine("No Permissions Found");
+
+            Console.WriteLine(string.Format("Supports Any Density: {0}", info.supportAnyDensity));
+            Console.WriteLine(string.Format("Supports Large Screens: {0}", info.supportLargeScreens));
+            Console.WriteLine(string.Format("Supports Normal Screens: {0}", info.supportNormalScreens));
+            Console.WriteLine(string.Format("Supports Small Screens: {0}", info.supportSmallScreens));
+#endif
+            return info;
+        }
+    }
+}
